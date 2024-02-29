b577556efc39e16c11c676ecae365d5247ee2b72
Manuel Perez Yebra <154430764+ManuelPerezYebra@users.noreply.github.com>
2024-02-29 10:12:40 +0100

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import { GlobalStyles } from "./GlobalStyles/GlobalStyles";
+
 const App = () => {
-	return <h1>Núcleo de la aplicación</h1>;
+	return <>
+	<GlobalStyles></GlobalStyles>
+	<h1>Núcleo de la aplicación</h1>;
+	
+	
+	</> 
+	
 };
 
 export default App;
